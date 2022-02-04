1128fd490f7f9a0906827e8b63ee59b50fe09c0b
unknown <muhammadelanghardifal>
2022-02-04 22:54:53 +0700

    initials commit

---
+++ src/components/todo-list/TodoList.js
@@ -0,0 +1,14 @@
+import './TodoList.css';
+
+const TodoList = (props) => {
+    return (
+        <ul>{
+            props.dataTodos.map((todo) => {
+                // {id: 1, title:'eat' }
+                return <li key={todo.id}>{todo.title}</li>
+            })
+        }</ul>
+    )
+}
+
+export default TodoList;
